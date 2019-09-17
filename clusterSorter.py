c374271d4c5c2935ebe5b3a55ee3e903ac367f17
Asanda2019 <55442291+Asanda2019@users.noreply.github.com>
2019-09-17 14:10:40 +0200

    Add files via upload

---
+++ clusterSorter.py
@@ -0,0 +1,41 @@
+
+
+# Libraries used.
+import sys
+
+# Tackling the sentences dump.
+
+sentenceFile = open(sys.argv[1])
+sentences = sentenceFile.readlines()
+sentenceFile.close()
+
+for idx in range(len(sentences)):
+    sentences[idx] = sentences[idx].split('\n')[0]
+
+# Creating cluster number index.
+
+clusterFile = open(sys.argv[2])
+clusterIndex = clusterFile.readlines()
+clusterFile.close()
+
+for idx in range(len(clusterIndex)):
+    clusterIndex[idx] = clusterIndex[idx].split('\n')[0]
+
+# Merging the 2 together.
+
+clusterSentenceIndex = []
+for idx in range(len(clusterIndex)):
+    temp = []
+    temp.append(clusterIndex[idx])
+    temp.append(sentences[idx])
+
+    clusterSentenceIndex.append(temp)
+
+clusterSentenceIndex.sort()
+
+# Printing the sentences into the file.
+outputIndexFile= open('sentence-cluster-sorted-index.txt','w')
+for idx in range(len(clusterSentenceIndex)):
+    line = clusterSentenceIndex[idx][1]+'$'+clusterSentenceIndex[idx][0]+'\n'
+    outputIndexFile.write(line)
+outputIndexFile.close()
